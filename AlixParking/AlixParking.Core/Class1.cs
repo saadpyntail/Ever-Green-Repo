051bce59a0c667a018dc4b7fe4e2576d7d9891ba
saadpyntail <saad@pyntail.com>
2016-07-14 12:16:28 +0500

    test comments

---
+++ AlixParking/AlixParking.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlixParking.Core
+{
+    public class Class1
+    {
+    }
+}
